e7f187e807e2593ca908590075b3b7438ca39a5a
gffrancheska <gffrancheska@gmail.com>
2017-05-20 14:32:09 -0400

    Adding some style.

--- src/components/TweedrFeed.js
+++ src/components/TweedrFeed.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import '../index.css';
 
 // Holds Tweed components, which reflect the 
 // information in the database.
 class TweedrFeed extends Component {
   render() {
     return (
-      <div></div>
+      <div className="ShowTweedr"></div>
 	);
   }
 }
