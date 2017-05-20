ee012b33811d702e6f39a49d78683d40b27a5f2a
gffrancheska <gffrancheska@gmail.com>
2017-05-20 14:00:36 -0400

    Adding input field.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import '../index.css';
 
 // The Input component will take a text input, and allow 
 // it to be submitted into the database.
@@ -8,7 +9,7 @@ class Input extends Component {
     return (
   	  <div className="createTweedr">
   	  	<input type="text" name="tweedr" placeholder="Tweedr here!" />
-      	<button>Submit</button>
+      	<button id="submit">Submit</button>
       </div>
     );
   }
