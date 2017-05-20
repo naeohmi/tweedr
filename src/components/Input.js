125ef65ca360b4518837658fcbedf397fa6ae322
gffrancheska <gffrancheska@gmail.com>
2017-05-20 13:37:44 -0400

    Adding comments.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// The Input component will take a text input, and allow 
+// it to be submitted into the database.
 class Input extends Component {
   render() {
     return (
   	  <div className="createTweedr">
-  	  <input type="text" name="tweedr" placeholder="Tweedr here!" />
-      <button>Submit</button>
+  	  	<input type="text" name="tweedr" placeholder="Tweedr here!" />
+      	<button>Submit</button>
       </div>
     );
   }
--- src/components/Tweed.js
+++ src/components/Tweed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// Holds one individual tweed.
 class Tweed extends Component {
   render() {
     return (
